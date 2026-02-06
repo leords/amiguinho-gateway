0d12362d5d1da37766f4c4fb59f5b15f866f342f
leords <leo_ro.drigues@hotmail.com>
2026-02-06 11:24:24 -0300

    teste

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -3,6 +3,10 @@ import { Router } from "express";
 
 const rotas = Router();
 
+rotas.get('/', (req, res) => {
+    res.send('API OK')
+})
+
 rotas.post("/validar-envio", new validarPedidosControlador().tratar)
 
 export { rotas }
